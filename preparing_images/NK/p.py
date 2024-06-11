292f90df231c93490027cb72e71cc224f056d83c
Nikodem Korohoda <nikodemko@interia.pl>
2024-06-11 17:37:51 +0200

    done

--- preparing_images/NK/p.py
+++ preparing_images/NK/p.py
@@ -7,7 +7,7 @@ OUTPUT = 'img/'
 def main():
     for filename in os.listdir(INPUT):
         im = Image.open(INPUT + filename)
-        im = im.crop((10, 10, 910, 910))
+        im = im.crop((500, 500, 1400, 1400))
         im.save(OUTPUT + filename)
 
 if __name__ == '__main__':
