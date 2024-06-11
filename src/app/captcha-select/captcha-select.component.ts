2d0496f422920b28acd823c2a999ed5321374412
Jakub <jakub.cudak@student.pk.edu.pl>
2024-06-11 21:33:31 +0200

    [KC] photos are randomly selected and visible in correct places

--- src/app/captcha-select/captcha-select.component.ts
+++ src/app/captcha-select/captcha-select.component.ts
@@ -1,4 +1,5 @@
-import {Component, Input} from '@angular/core';
+// captcha-select.component.ts
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {MatCheckbox} from "@angular/material/checkbox";
 import {ImageModel} from "./image-model";
 import {CommonModule} from "@angular/common";
@@ -6,54 +7,66 @@ import {MatButton} from "@angular/material/button";
 import {MatDialog} from "@angular/material/dialog";
 import {SubmitModalComponent} from "../submit-modal/submit-modal.component";
 
+interface ImageData {
+    photoName: string;
+    isCorrect: boolean;
+}
+
 @Component({
-  selector: 'app-captcha-select',
-  standalone: true,
-  imports: [
-    MatCheckbox,
-    CommonModule,
-    MatButton
-  ],
-  templateUrl: './captcha-select.component.html',
-  styleUrl: './captcha-select.component.scss',
+    selector: 'app-captcha-select',
+    standalone: true,
+    imports: [
+        MatCheckbox,
+        CommonModule,
+        MatButton
+    ],
+    templateUrl: './captcha-select.component.html',
+    styleUrl: './captcha-select.component.scss',
 })
-export class CaptchaSelectComponent {
+export class CaptchaSelectComponent implements OnChanges {
 
-  @Input()
-  public stageName: string = '';
+    @Input()
+    public stageName: string = '';
 
-  @Input()
-  public images: ImageModel[] = [new ImageModel(), new ImageModel(), new ImageModel(), new ImageModel(), new ImageModel(),
-    new ImageModel(), new ImageModel(), new ImageModel(), new ImageModel()];
+    @Input()
+    public imageNames: ImageData[] = [];
 
-  public selectedImgListLength: number = 0;
+    public images: ImageModel[] = [];
 
-  constructor(private dialog: MatDialog) {}
+    public selectedImgListLength: number = 0;
 
-  public onClickImage(img: ImageModel) {
-    if (img.isSelected) {
-      img.isSelected = false;
-    } else {
-      this.selectedImgListLength = this.images.filter((i: ImageModel) => i.isSelected).length;
-
-      if (this.selectedImgListLength < 5) {
-        img.isSelected = true;
-      }
+    constructor(private dialog: MatDialog) {
     }
 
-    this.selectedImgListLength = this.images.filter((i: ImageModel) => i.isSelected).length;
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes['imageNames'] && changes['imageNames'].currentValue) {
+            this.images = this.imageNames.map(data => new ImageModel('assets/images/' + data.photoName, data.isCorrect));
+        }
+    }
 
-  }
+    public onClickImage(img: ImageModel) {
+        if (img.isSelected) {
+            img.isSelected = false;
+        } else {
+            this.selectedImgListLength = this.images.filter((i: ImageModel) => i.isSelected).length;
 
-  public onConfirm() {
-    const dialogRef = this.dialog.open(SubmitModalComponent, {
-      data: {
-        title: 'You selected right images!',
-        subtitle: 'You can now go to the next stage',
-        nextButtonActive: true,
-        retryButtonActive: true
-      }
-    })
-  }
+            if (this.selectedImgListLength < 5) {
+                console.log('dupa: ' + img.isCorrect)
+                img.isSelected = true;
+            }
+        }
 
+        this.selectedImgListLength = this.images.filter((i: ImageModel) => i.isSelected).length;
+    }
+
+    public onConfirm() {
+        const dialogRef = this.dialog.open(SubmitModalComponent, {
+            data: {
+                title: 'You selected right images!',
+                subtitle: 'You can now go to the next stage',
+                nextButtonActive: true,
+                retryButtonActive: true
+            }
+        })
+    }
 }
